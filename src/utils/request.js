59aedfe5727669184078132d9eedaca6a8923830
linwinwin <497378774@qq.com>
2022-07-17 17:45:23 +0800

    完善了拒绝的逻辑和弹框样式

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,8 +2,7 @@
 import axios from "axios";
 import {
     Message
-} from "element-ui";
-
+} from 'element-ui';
 const ins = axios.create() //  创建一个 axios 实例
 // 设置请求响应拦截
 ins.interceptors.response.use(function (resp) {
@@ -12,9 +11,12 @@ ins.interceptors.response.use(function (resp) {
     } else if (resp.data.status === 10) {
         window.location.href('/#/login')
     } else {
-        alert("发生了错误")
+        Message.error({
+            message: resp.data.msg
+        })
+        return Promise.reject(resp)
     }
 })
 ins.defaults.baseURL = '/api' // 设置默认请求时间
-ins.defaults.timeout = 8000 // 设置请求超时时间为 8 秒
+// ins.defaults.timeout = 8000 // 设置请求超时时间为 8 秒
 export default ins
