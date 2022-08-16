7e6e78d04994a33d2c01f3434a498986f219e683
linwinwin <497378774@qq.com>
2022-08-16 20:16:26 +0800

    新增订单相关接口

---
+++ src/api/order.js
@@ -0,0 +1,25 @@
+import request from "@/utils/request"
+//  创建一个订单
+export async function addOrder(shippingId) {
+    return request.post('/orders', {
+        shippingId
+    })
+}
+
+// 获取订单列表
+export async function deleteOrder(pageSize = 10, pageNum = 1) {
+    return request.get("/orders", {
+        pageSize,
+        pageNum
+    })
+}
+
+//获取订单详情
+export async function getOrderDetails(orderNo) {
+    return request.get(`/orders/${orderNo}`)
+}
+
+//取消订单
+export async function cancleOrder(orderNo) {
+    return request.put(`/orders/${orderNo}`)
+}
